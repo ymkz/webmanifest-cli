0d8911d88537c38d6fbba2de811a1c0787d7a2cb
ymkz <ymkzly@gmail.com>
2019-12-10 19:24:25 +0900

    refactor(cli): change cli module to meow

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 import { pathExists, readJSON, writeJSON } from 'fs-extra'
-import { cli, option } from 'typed-cli'
 import { imageSize } from 'image-size'
 import sharp from 'sharp'
+import meow from 'meow'
 
 const SPLIT_TARGET_CHAR = ','
 const MIN_ACCEPT_IMAGE_RESOLUTION = 512
@@ -12,38 +12,57 @@ const DEFAULT_OUTPUT_PATH = 'public'
 const DEFAULT_OUTPUT_SIZES = '512,384,192,180,152,144,128,96,72'
 
 !(async () => {
-  const { options } = cli({
-    options: {
-      icon: option.string
-        .alias('i')
-        .description('icon file')
-        .required(),
-      manifest: option.string
-        .alias('m')
-        .description('manifest file')
-        .default(DEFAULT_WEBMANIFEST_PATH),
-      output: option.string
-        .alias('o')
-        .description('output path')
-        .default(DEFAULT_OUTPUT_PATH),
-      sizes: option.string
-        .alias('s')
-        .description('icon sizes')
-        .default(DEFAULT_OUTPUT_SIZES)
+  const cli = meow(
+    `
+  Usage
+    $ webmanifest --icon <filepath>
+
+  Options
+    --icon, -i Template icon file <required>
+    --manifest, -m Template webmanifest file
+    --output, -o Output directory path
+    --sizes, -s Output icon sizes
+
+  Examples
+    $ webmanifest --icon public/icon.svg
+    $ webmanifest --icon public/icon.svg --sizes 512,192,96,32,16
+  `,
+    {
+      flags: {
+        icon: {
+          type: 'string',
+          alias: 'i'
+        },
+        manifest: {
+          type: 'string',
+          alias: 'm',
+          default: DEFAULT_WEBMANIFEST_PATH
+        },
+        output: {
+          type: 'string',
+          alias: 'o',
+          default: DEFAULT_OUTPUT_PATH
+        },
+        sizes: {
+          type: 'string',
+          alias: 's',
+          default: DEFAULT_OUTPUT_SIZES
+        }
+      }
     }
-  })
+  )
 
-  if (!(await pathExists(options.icon))) {
-    throw new Error(`${options.icon} does not exists.`)
+  if (!(await pathExists(cli.flags.icon))) {
+    throw new Error(`${cli.flags.icon} does not exists.`)
   }
-  if (!(await pathExists(options.manifest))) {
-    throw new Error(`${options.manifest} does not exists.`)
+  if (!(await pathExists(cli.flags.manifest))) {
+    throw new Error(`${cli.flags.manifest} does not exists.`)
   }
-  if (!(await pathExists(options.output))) {
-    throw new Error(`${options.output} does not exists.`)
+  if (!(await pathExists(cli.flags.output))) {
+    throw new Error(`${cli.flags.output} does not exists.`)
   }
 
-  const iconDimension = imageSize(options.icon)
+  const iconDimension = imageSize(cli.flags.icon)
 
   if (!iconDimension.width || !iconDimension.height) {
     throw new Error('Icon file is invalid.')
@@ -60,28 +79,28 @@ const DEFAULT_OUTPUT_SIZES = '512,384,192,180,152,144,128,96,72'
 
   const ascendingOrder = (a: number, b: number) => (a < b ? 1 : -1)
 
-  const outputSizes = options.sizes.includes(SPLIT_TARGET_CHAR)
-    ? options.sizes
+  const outputSizes = cli.flags.sizes.includes(SPLIT_TARGET_CHAR)
+    ? cli.flags.sizes
         .split(SPLIT_TARGET_CHAR)
         .map(Number)
         .filter(Number)
         .sort(ascendingOrder)
-    : [Number(options.sizes)].filter(Number)
+    : [Number(cli.flags.sizes)].filter(Number)
 
   try {
     for (const size of outputSizes) {
-      await sharp(options.icon)
+      await sharp(cli.flags.icon)
         .resize(size, size)
         .png()
-        .toFile(`${options.output}/icon-${size}x${size}.png`)
-      console.log(`Output: ${options.output}/icon-${size}x${size}.png`)
+        .toFile(`${cli.flags.output}/icon-${size}x${size}.png`)
+      console.log(`Output: ${cli.flags.output}/icon-${size}x${size}.png`)
     }
   } catch (error) {
     throw error
   }
 
-  const inputManifest = await readJSON(options.manifest)
-  const outputManifest = {
+  const inputManifest = await readJSON(cli.flags.manifest)
+  const outputManifestContent = {
     ...inputManifest,
     icons: outputSizes.map(size => ({
       src: `icon-${size}x${size}.png`,
@@ -90,8 +109,8 @@ const DEFAULT_OUTPUT_SIZES = '512,384,192,180,152,144,128,96,72'
     }))
   }
 
-  await writeJSON(options.manifest, outputManifest, { spaces: 2 })
-  console.log(`Output: ${options.manifest}`)
+  await writeJSON(cli.flags.manifest, outputManifestContent, { spaces: 2 })
+  console.log(`Output: ${cli.flags.manifest}`)
 
   console.log('\n🎉 Done! All icons and manifest file is generated successfully.\n')
 })()
