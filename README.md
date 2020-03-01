52b22478da98f27ae25a31e688cec13035d11f81
ymkz <ymkzly@gmail.com>
2020-03-02 02:29:12 +0900

    feat(breaking-change): upgrade codebase and dependencies

--- README.md
+++ README.md
@@ -1,55 +1,25 @@
-<div>
-  <h1>webmanifest-cli</h1>
-</div>
+# webmanifest-cli
 
 <p>
-  <a
-    href="https://npmjs.com/package/webmanifest-cli"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+  <a href="https://npmjs.com/package/webmanifest-cli" target="_blank" rel="noopener noreferrer">
     <img src="https://flat.badgen.net/npm/v/webmanifest-cli?icon=npm" />
   </a>
-  <a
-    href="https://npmjs.com/package/webmanifest-cli"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+  <a href="https://npmjs.com/package/webmanifest-cli" target="_blank" rel="noopener noreferrer">
     <img src="https://flat.badgen.net/npm/types/webmanifest-cli?icon=npm" />
   </a>
-  <a
-    href="https://github.com/ymkz/webmanifest-cli"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+  <a href="https://github.com/ymkz/webmanifest-cli" target="_blank" rel="noopener noreferrer">
     <img src="https://flat.badgen.net/github/checks/ymkz/webmanifest-cli?icon=github" />
   </a>
-  <a
-    href="https://app.dependabot.com/accounts/ymkz/repos/214224516"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+  <a href="https://app.dependabot.com/accounts/ymkz/repos/214224516" target="_blank" rel="noopener noreferrer">
     <img src="https://flat.badgen.net/dependabot/ymkz/webmanifest-cli?icon=dependabot" />
   </a>
-  <a
-    href="https://libraries.io/npm/webmanifest-cli"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+  <a href="https://libraries.io/npm/webmanifest-cli" target="_blank" rel="noopener noreferrer">
     <img src="https://img.shields.io/librariesio/release/npm/webmanifest-cli?style=flat-square" />
   </a>
-  <a
-    href="https://prettier.io"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+  <a href="https://prettier.io" target="_blank" rel="noopener noreferrer">
     <img src="https://flat.badgen.net/badge/formatter/prettier/ff69b4" />
   </a>
-  <a
-    href="https://twitter.com/ymkzly"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+  <a href="https://twitter.com/ymkzly" target="_blank" rel="noopener noreferrer">
     <img src="https://flat.badgen.net/twitter/follow/ymkzly?icon=twitter" />
   </a>
 </p>
@@ -69,7 +39,8 @@ npm i --save-dev webmanifest-cli
 ## Usage
 
 ```sh
-webmanifest --icon icon.svg
+$ webmanifest
+$ webmanifest --icon public/icon.svg --manifest public/manifest.webmanifest --outputIcon public/assets --outputManifest public/manifest.webmanifest --sizes 512,384,192,180,152,144,128,96,72
 ```
 
 ## Author
@@ -89,7 +60,7 @@ Give a ⭐️ if this project helped you!
 
 ## 📝 License
 
-Copyright © 2019 [ymkz](https://github.com/ymkz).<br />
+Copyright © 2020 [ymkz](https://github.com/ymkz).<br />
 This project is [MIT](https://github.com/ymkz/webmanifest-cli/blob/master/LICENSE) licensed.
 
 ---
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@
 
 import { pathExists, readJSON, writeJSON } from 'fs-extra'
 import { imageSize } from 'image-size'
-import sharp from 'sharp'
 import meow from 'meow'
+import sharp from 'sharp'
 
-const SPLIT_TARGET_CHAR = ','
+const ICON_SIZE_DELIMITER = ','
 const MIN_ACCEPT_IMAGE_RESOLUTION = 512
-const DEFAULT_WEBMANIFEST_PATH = 'public/manifest.webmanifest'
-const DEFAULT_OUTPUT_PATH = 'public'
+const DEFAULT_INPUT_ICON_PATH = 'public/icon.svg'
+const DEFAULT_INPUT_MANIFEST_PATH = 'public/manifest.webmanifest'
+const DEFAULT_OUTPUT_ICON_PATH = 'public/assets'
+const DEFAULT_OUTPUT_MANIFEST_PATH = 'public/manifest.webmanifest'
 const DEFAULT_OUTPUT_SIZES = '512,384,192,180,152,144,128,96,72'
 
 !(async () => {
@@ -20,35 +22,42 @@ const DEFAULT_OUTPUT_SIZES = '512,384,192,180,152,144,128,96,72'
   Options
     --icon, -i Template icon file <required>
     --manifest, -m Template webmanifest file
-    --output, -o Output directory path
+    --outputIcon, -oi Output icons directory path
+    --outputManifest, -om Output webmanifest directory path
     --sizes, -s Output icon sizes
 
   Examples
-    $ webmanifest --icon public/icon.svg
-    $ webmanifest --icon public/icon.svg --sizes 512,192,96,32,16
+    $ webmanifest
+    $ webmanifest --icon public/icon.svg --manifest public/manifest.webmanifest --outputIcon public/assets --outputManifest public/manifest.webmanifest --sizes 512,384,192,180,152,144,128,96,72
   `,
     {
       flags: {
         icon: {
           type: 'string',
-          alias: 'i'
+          alias: 'i',
+          default: DEFAULT_INPUT_ICON_PATH,
         },
         manifest: {
           type: 'string',
           alias: 'm',
-          default: DEFAULT_WEBMANIFEST_PATH
+          default: DEFAULT_INPUT_MANIFEST_PATH,
         },
-        output: {
+        outputIcon: {
           type: 'string',
-          alias: 'o',
-          default: DEFAULT_OUTPUT_PATH
+          alias: 'oi',
+          default: DEFAULT_OUTPUT_ICON_PATH,
+        },
+        outputManifest: {
+          type: 'string',
+          alias: 'om',
+          default: DEFAULT_OUTPUT_MANIFEST_PATH,
         },
         sizes: {
           type: 'string',
           alias: 's',
-          default: DEFAULT_OUTPUT_SIZES
-        }
-      }
+          default: DEFAULT_OUTPUT_SIZES,
+        },
+      },
     }
   )
 
@@ -58,45 +67,44 @@ const DEFAULT_OUTPUT_SIZES = '512,384,192,180,152,144,128,96,72'
   if (!(await pathExists(cli.flags.manifest))) {
     throw new Error(`${cli.flags.manifest} does not exists.`)
   }
-  if (!(await pathExists(cli.flags.output))) {
-    throw new Error(`${cli.flags.output} does not exists.`)
+  if (!(await pathExists(cli.flags.outputIcon))) {
+    throw new Error(`${cli.flags.outputIcon} does not exists.`)
+  }
+  if (!(await pathExists(cli.flags.outputManifest))) {
+    throw new Error(`${cli.flags.outputManifest} does not exists.`)
   }
 
-  const iconDimension = imageSize(cli.flags.icon)
+  const iconRectangle = imageSize(cli.flags.icon)
 
-  if (!iconDimension.width || !iconDimension.height) {
+  if (!iconRectangle.width || !iconRectangle.height) {
     throw new Error('Icon file is invalid.')
   }
-  if (iconDimension.width !== iconDimension.height) {
+  if (iconRectangle.width !== iconRectangle.height) {
     throw new Error('Icon file is required its square.')
   }
-  if (iconDimension.width && iconDimension.width < MIN_ACCEPT_IMAGE_RESOLUTION) {
+  if (iconRectangle.width && iconRectangle.width < MIN_ACCEPT_IMAGE_RESOLUTION) {
     throw new Error(`Icon file is required larger than ${MIN_ACCEPT_IMAGE_RESOLUTION}px.`)
   }
-  if (iconDimension.height && iconDimension.height < MIN_ACCEPT_IMAGE_RESOLUTION) {
+  if (iconRectangle.height && iconRectangle.height < MIN_ACCEPT_IMAGE_RESOLUTION) {
     throw new Error(`Icon file is required larger than ${MIN_ACCEPT_IMAGE_RESOLUTION}px.`)
   }
 
   const ascendingOrder = (a: number, b: number) => (a < b ? 1 : -1)
 
-  const outputSizes = cli.flags.sizes.includes(SPLIT_TARGET_CHAR)
+  const outputSizes = cli.flags.sizes.includes(ICON_SIZE_DELIMITER)
     ? cli.flags.sizes
-        .split(SPLIT_TARGET_CHAR)
+        .split(ICON_SIZE_DELIMITER)
         .map(Number)
         .filter(Number)
         .sort(ascendingOrder)
     : [Number(cli.flags.sizes)].filter(Number)
 
-  try {
-    for (const size of outputSizes) {
-      await sharp(cli.flags.icon)
-        .resize(size, size)
-        .png()
-        .toFile(`${cli.flags.output}/icon-${size}x${size}.png`)
-      console.log(`Output: ${cli.flags.output}/icon-${size}x${size}.png`)
-    }
-  } catch (error) {
-    throw error
+  for (const size of outputSizes) {
+    await sharp(cli.flags.icon)
+      .resize(size, size)
+      .png()
+      .toFile(`${cli.flags.outputIcon}/icon-${size}x${size}.png`)
+    console.log(`Output icon: ${cli.flags.outputIcon}/icon-${size}x${size}.png`)
   }
 
   const inputManifest = await readJSON(cli.flags.manifest)
@@ -105,12 +113,12 @@ const DEFAULT_OUTPUT_SIZES = '512,384,192,180,152,144,128,96,72'
     icons: outputSizes.map(size => ({
       src: `icon-${size}x${size}.png`,
       sizes: `${size}x${size}`,
-      type: 'image/png'
-    }))
+      type: 'image/png',
+    })),
   }
 
-  await writeJSON(cli.flags.manifest, outputManifestContent, { spaces: 2 })
-  console.log(`Output: ${cli.flags.manifest}`)
+  await writeJSON(cli.flags.outputManifest, outputManifestContent, { spaces: 2 })
+  console.log(`Output webmanifest: ${cli.flags.outputManifest}`)
 
-  console.log('\n🎉 Done! All icons and manifest file is generated successfully.\n')
+  console.log('\n🎉 All icons and webmanifest file is generated successfully.\n')
 })()
